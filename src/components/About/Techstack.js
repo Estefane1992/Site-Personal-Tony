5d8a9866cd58dca774ac9a60e333cf9c112f192e
Estefane1992 <108195915+Estefane1992@users.noreply.github.com>
2023-03-31 11:29:02 -0300

    Add files via upload

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -7,7 +7,7 @@ function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col md={5} className="card-missao">
-        <span data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">MISSÃO</span><br/><br/>
+        <span  data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">MISSÃO</span><br/><br/>
         <p style={{textAlign: "justify"}} data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">Oferecer o que existe de mais eficiente na área de preparação física, proporcionando uma nova experiência a cada dia de treinamento</p>
         
       </Col>
